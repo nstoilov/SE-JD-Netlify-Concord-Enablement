4fdd29c0d17b8bb4918356facd86ed24a38b238e
John Running <john.a.running@gmail.com>
2023-04-24 16:00:50 -0400

    first commit

---
+++ src/canvas/index.ts
@@ -0,0 +1,19 @@
+import './CallToAction';
+import './Hero';
+import './Article';
+import './ArticlesList';
+import './ArticleListItem';
+import './Container';
+import './SectionTwoColumns';
+import './AddToCart';
+import './ProductInfo';
+import './ProductDescription';
+import './ProductImageGallery';
+import './FeaturedProducts';
+import './RecommendedArticles';
+import './Product';
+
+import './AlgoliaSearch/AlgoliaHits';
+import './AlgoliaSearch/AlgoliaRefinementList';
+import './AlgoliaSearch/AlgoliaSearchBox';
+import './AlgoliaSearch/AlgoliaInstantSearch';
